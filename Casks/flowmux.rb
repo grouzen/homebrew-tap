eacb76c0ca974d5dfb5f03b19e8ec548d27eb920
Michael Nedokushev <grouzen.hexy@gmail.com>
2026-07-01 20:10:16 +0200

    chore(ci): migrate to macos universal binary

--- Casks/flowmux.rb
+++ Casks/flowmux.rb
@@ -2,7 +2,7 @@ cask "flowmux" do
   version "0.1.2"
   sha256 "c82b962f4928f8a24b04eed14326dcc71ce502f23e3599aba57bdd17a92573fa"
 
-  url "https://github.com/grouzen/flowmux/releases/download/v#{version}/flowmux-v#{version}-aarch64-apple-darwin.tar.gz"
+  url "https://github.com/grouzen/flowmux/releases/download/v#{version}/flowmux-v#{version}-universal2-apple-darwin.tar.gz"
   name "Flowmux"
   desc "Terminal-native AI agent multiplexer"
   homepage "https://github.com/grouzen/flowmux"
@@ -12,8 +12,7 @@ cask "flowmux" do
     strategy :github_latest
   end
 
-  depends_on arch: :arm64
   depends_on formula: "tmux"
 
-  binary "flowmux-v#{version}-aarch64-apple-darwin/flowmux"
+  binary "flowmux-v#{version}-universal2-apple-darwin/flowmux"
 end
